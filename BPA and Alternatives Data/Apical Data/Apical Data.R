4152462e557d83120386304f107a318dd71a0f2f
tylernguyen1997 <tylernguyen@cmail.carleton.ca>
2022-03-21 19:18:56 -0400

    Fixed Apical Data to include Deformity Stuff

--- BPA and Alternatives Data/Apical Data/Apical Data.R
+++ BPA and Alternatives Data/Apical Data/Apical Data.R
@@ -16,34 +16,58 @@ for(i in chemnames){
   apicaldata[[i]]$Dose <- as.factor(apicaldata[[i]]$Dose)
 }
 
-anova_output <- sapply(apicaldata, combinedanova)
-print(anova_output)
+lethal_anova_output <- sapply(apicaldata, combinedanova, type = "lethal")
+print(lethal_anova_output)
 
-dunnett_output <- sapply(apicaldata, combineddunnett)
-print(dunnett_output)
+deform_anova_output <- sapply(apicaldata, combinedanova, type = "deform")
+print(deform_anova_output)
+
+lethal_dunnett_output <- sapply(apicaldata, combineddunnett, type = "lethal")
+print(lethal_dunnett_output)
+
+deform_dunnett_output<- sapply(apicaldata, combineddunnett, type = "deform")
+print(deform_dunnett_output)
 
 summarystats_output <- lapply(apicaldata, summarystats)
 
 
 ####Plotting####
-
-apicalplots <- list()
+#lethal plots
+lethal_plots <- list()
 
 for(i in chemnames){
-  apicalplots[[i]] <-
+  lethal_plots[[i]] <-
     summarystats_output[[i]] %>%
     ggplot(aes(x = as.character(Dose), y = Survival_Rate, group = Dose)) +
     geom_bar(stat="identity", color ="black", fill="white", 
              position=position_dodge()) +
-    geom_errorbar(aes(ymin=Survival_Rate-StdDev, ymax=Survival_Rate+StdDev), width=.2,
+    geom_errorbar(aes(ymin=Survival_Rate-Survival_StdDev, ymax=Survival_Rate+Survival_StdDev), width=.2,
                   position=position_dodge(.9)) +
     theme_classic() +
     xlab("Dose (µg/L)") +
     ylab("Survival Rate") +
-    scale_y_continuous(limits = c(0,1.1), breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1)) +
+    scale_y_continuous(limits = c(-0.1,1.1), breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1)) +
     labs(title = i)
 }
 
-# print(apicalplots[[1]])
+multiplot(plotlist = lethal_plots, cols = 2)
 
-multiplot(plotlist = apicalplots, cols = 2)
+#deform plots
+deform_plots <- list()
+
+for(i in chemnames){
+  deform_plots[[i]] <-
+    summarystats_output[[i]] %>%
+    ggplot(aes(x = as.character(Dose), y = Deform_Rate, group = Dose)) +
+    geom_bar(stat="identity", color ="black", fill="white", 
+             position=position_dodge()) +
+    geom_errorbar(aes(ymin=Deform_Rate-Deform_StdDev, ymax=Deform_Rate+Deform_StdDev), width=.2,
+                  position=position_dodge(.9)) +
+    theme_classic() +
+    xlab("Dose (µg/L)") +
+    ylab("Deformity Rate") +
+    scale_y_continuous(limits = c(-0.1,1.1), breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1)) +
+    labs(title = i)
+}
+
+multiplot(plotlist = deform_plots, cols = 2)
