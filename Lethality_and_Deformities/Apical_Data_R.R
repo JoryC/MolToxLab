9e54009ead0b859938b229b531521c90e55b450b
JoryCurry <jorycurry@cmail.carleton.ca>
2022-03-21 16:34:09 -0400

    Added stashed changes to apical data in the master

--- Lethality_and_Deformities/Apical_Data_R.R
+++ Lethality_and_Deformities/Apical_Data_R.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(ggplot2)
 ### FOLDERS, FILES AND FUNCTIONS
-setwd("~/MolToxLab/Lethality_and_Deformities/") #Please set your own working directory
+# setwd("~/MolToxLab/Lethality_and_Deformities/") # Please set your own working directory
 temp1 <- list.files("Apical_Data_Sheets/", "*.csv") #List all of the files in the working directory
 apicaldata <- lapply(paste0("Apical_Data_Sheets/", temp1), read.csv) #Import the data to a single object
 rm(temp1)
@@ -55,6 +55,7 @@ print(apicaldatasummary)
 ##################
 
 #Survival Rate
+#Saving Images
 for(i in 1:length(apicaldatasummary)) {
   p <- apicaldatasummary[[i]] %>%
     as.data.frame() %>%
@@ -70,7 +71,7 @@ for(i in 1:length(apicaldatasummary)) {
     xlab("Dose (mg/L)") +
     ylab("Survival Rate")
     print(p)
-    ggsave(names(apicaldatasummary[i]), device = "pdf", path = paste0(getwd(), "/Images/Survival_Rate"))
+    ggsave(paste0(names(apicaldatasummary[i]), ".png"), device = "png", path = paste0(getwd(), "/Images/Survival_Rate"))
 }
 
 #Deformity Rate
@@ -87,5 +88,24 @@ for(i in 1:length(apicaldatasummary)) {
     xlab("Dose (mg/L)") +
     ylab("Rate of Deformity")
   print(p)
-  ggsave(names(apicaldatasummary[i]), device = "pdf", path = paste0(getwd(), "/Images/Deformity_Rate"))
+  ggsave(paste0(names(apicaldatasummary[i]), ".png"), device = "png", path = paste0(getwd(), "/Images/Deformity_Rate"))
+}
+
+#Hatch Rate
+for(i in 1:length(apicaldatasummary)) {
+  p <- apicaldatasummary[[i]] %>%
+    as.data.frame() %>%
+    mutate(Dose = as.factor(Dose)) %>%
+    ggplot(aes(x=Dose, y=Hatch_Rate)) +
+    geom_bar(stat="identity", color="black", fill="white", 
+             position=position_dodge()) +
+    geom_errorbar(aes(ymin=Hatch_Rate-HR_SD, ymax=Hatch_Rate+HR_SD), width=.2,
+                  position=position_dodge(.9)) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+    labs(title=names(apicaldatasummary)[i]) +
+    xlab("Dose (mg/L)") +
+    ylab("Hatch Rate")
+  print(p)
+  ggsave(names(apicaldatasummary[i]), device = "pdf", path = paste0(getwd(), "/Images/Hatch_Rate"))
 }
