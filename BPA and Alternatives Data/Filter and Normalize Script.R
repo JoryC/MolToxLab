0d2e23d1c9ad5435c317eb52a6f62422d53b518d
tylernguyen1997 <tylernguyen@cmail.carleton.ca>
2022-03-25 12:07:17 -0400

    Update Filter and Normalize Script.R

--- BPA and Alternatives Data/Filter and Normalize Script.R
+++ BPA and Alternatives Data/Filter and Normalize Script.R
@@ -5,6 +5,7 @@ library(preprocessCore) # for quantile normalization
 library(PMCMRplus)  # for Williams Trend Test
 # library(purrr)
 library(ggfortify)
+options(scipen = 9)
 
 source("RNAseqFunctions.R")
 source("BMDExpressFunctions.R") # for multiplot
@@ -53,12 +54,86 @@ allData <- reduce(loadRaw, full_join, by="gene") %>%
 nestData <- allData %>%
   nest()
   
+####QC####
+# Number of genes with at least 1 read per sample
+ngene_per_sample <- allData %>% 
+  as.data.frame() %>% 
+  mutate(genecount = rowSums(.[4:ncol(allData)]!=0)) %>%
+  select(sample, chemical, dose, genecount)
+
+ngene_per_sample_mean <- mean(ngene_per_sample[,"genecount"])
+
+ngene_per_chem_mean <- ngene_per_sample %>% 
+  select(-sample, -dose) %>%
+  group_by(chemical) %>% 
+  summarise_at(vars(genecount), .funs = c(mean, sd)) %>%
+  rename(ngene_mean = fn1) %>%
+  rename(ngene_sd = fn2)
+
+# Number of genes with at least 1 read across all samples
+ngene_total_data <- mapply(sum, allData[,-c(1:3)])
+ngene_total <- length(which(ngene_total_data != 0))
+
+# Number of genes with a cumulative read count of at least 5 across all samples
+ncov5 <- length(which(ngene_total_data >= 5))
+
+# Number of genes (highest to lowest read count) that make up 80% of reads
+nsig80<-vector()
+for(i in 1:5){
+  test1 <-nestData$data[[i]] %>%
+    select(-sample, -dose) %>%
+    colSums()
+  test1<- test1[order(test1, decreasing=TRUE)]
+  test_percent <- test1/sum(test1)*100
+  nsig80<-c(nsig80,sum(cumsum(test_percent)<80))
+  rm(test1, test_percent)
+}
+names(nsig80) <- as.vector(nestData$chemical)
+nsig80_mean <- mean(nsig80)
+
+#Number of genes that have at least 1 read across all samples per chemical
+ngene_total_chem_data <- vector()
+for(i in 1:5) {
+  temp <- nestData$data[[i]] %>%
+    select(-sample,-dose)
+  temp[temp > 0] <- 1
+  temp_sum <- mapply(sum, temp)
+  ngene_total_chem_data <-
+    c(ngene_total_chem_data, length(which(temp_sum == nrow(nestData$data[[i]]))))
+  rm(temp, temp_sum)
+}
+
+#Number of genes that have at least 1 read across all samples for all chemicals
+# ngene_total_allsamples_data <- allData %>%
+#   as.data.frame() %>%
+#   select(-sample, -chemical, -dose)
+# ngene_total_allsamples_data[ngene_total_allsamples_data > 0] <-1
+# ngene_total_allsamples_sum <- mapply(sum, ngene_total_allsamples_data)
+# ngene_total_allsamples <- length(which(ngene_total_allsamples_sum == nrow(allData)))
+
+#Overall QC Summary
+qcSummary <- data.frame(endpoint = c("ngene_per_sample_mean", 
+                                     "ngene_total",
+                                     # "ngene_total_allsamples",
+                                     "ncov5", 
+                                     "nsig80_mean"),
+                        value = c(ngene_per_sample_mean,
+                                  ngene_total,
+                                  # ngene_total_allsamples,
+                                  ncov5,
+                                  nsig80_mean)
+                        )
+
+#Chemical specific QC
+qcSummary_chem <- ngene_per_chem_mean %>%
+  select(-ngene_sd) %>%
+  mutate(ngene_allsamples = ngene_total_chem_data) %>%
+  mutate(nsig80 = nsig80)
 
 #### FILTER ####
-# uses the "countFiler' function from the "RNAseqFunctions" source code
+# uses the "countFilter' function from the "RNAseqFunctions" source code
 nestData <- nestData %>%
   mutate(filterData = map(data, countFilter, grouping = "dose", median_threshold = 5)) 
-
 
 #### NORMALIZE ####
 nestData <- nestData %>%
