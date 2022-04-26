804f0fdbe7f95d0e358e5b4040aa55b59d0fa482
jasonob1 <30319093+jasonob1@users.noreply.github.com>
2022-04-26 17:49:16 -0400

    updated filter functions and added reduced stringency filtering script
    also exported reduced stringency filtered and DeSeq normalized data

--- BPA and Alternatives Data/RNAseqFunctions.R
+++ BPA and Alternatives Data/RNAseqFunctions.R
@@ -1,37 +1,110 @@
+#### FUNCTION: cleanZeroRows
+cleanZeroRows <-function(myData, metadata){
+  require(tidyverse)
+
+  # column selectors: used to select all meta data columns, so that the gene cols can be quickly selected
+  metaCols <- names(metadata)[names(metadata) %in% names(myData)]
+  omitCols <- metaCols
+  
+  # select only the columns that are NOT all zero
+  cleanData <- myData %>%
+    select(-metaCols) %>%
+    select(where(~!all(.==0))) %>%
+    bind_cols((myData %>% select(all_of(metaCols))), . )
+ 
+  return(cleanData)
+}
+
+
 
 #### FUNCTION: countFilter #####
-countFilter <- function(data, grouping, median_threshold, metadata){
+countFilter <- function(myData, grouping, median_threshold, metadata){
   require(tidyverse)
- 
-  # column selectors: used to select all meta data columns, so that the gene cols can be quickly selected
-  metaCols <- names(metadata)[names(metadata) %in% names(data)]
-  omitCols <- metaCols[!metaCols %in% grouping]
   
-  # group and nest by "grouping"
-  nestData<-data %>%
-    dplyr::select(-all_of(omitCols)) %>%
-    group_by(across(all_of(grouping))) %>%
-    nest()
+  # column selectors: used to select all meta data columns, so that the gene cols can be quickly selected
+  metaCols <- names(metadata)[names(metadata) %in% names(myData)]
+  
+  if(grouping == "none"){
+    omitCols <- metaCols
+  }else{
+    omitCols <- metaCols[!metaCols %in% grouping]
+  }
+  
+  # optional group and nest by "grouping"
+  if(grouping == "none"){
+    nestedT<-myData %>%
+      dplyr::select(-all_of(omitCols)) %>%
+      tidyr::nest(data=everything())
+  }else{
+    nestedT<-myData %>%
+      dplyr::select(-all_of(omitCols)) %>%
+      group_by(across(all_of(grouping))) %>%
+      nest()
+  }
   
   # determine median count of each nested group
-  nestData <- nestData %>%
-    mutate(medCount = map(data, ~sapply(.x, median)))
-
-  medianCount <- as_tibble(do.call(rbind, nestData$medCount))
+  nestedT <- nestedT %>%
+    dplyr::mutate(medCount = map(data, ~sapply(.x, median)))
+  
+  medianCount <- as_tibble(do.call(rbind, nestedT$medCount))
   
   # minimum median count per gene
   minCount <- medianCount %>%
-    sapply(min)
+    base::sapply(min)
   
   # gene names that are above median_threshold
   keepGenes <- names(minCount)[which(minCount >= median_threshold)]
   
   # filter to only keep genes above median_threshold
-  filterData <- data %>%
+  filterData <- myData %>%
     dplyr::select(all_of(c(metaCols, keepGenes)))
-
+  
   return(filterData)
 }
+
+
+#### FUNCTION: DeSeq2 Normalization ####
+ # NOTE: input must be a nested tibble, column names must be characters (use quotes)
+deSeqNorm <- function(myTibble, normCol, metaCol, newCol){
+  require(DESeq2)
+  deSeqData<-list()
+  
+  for(i in 1:nrow(myTibble)){
+    
+    tData <-nestData[[normCol]][[i]] %>%
+      select(-dose) %>%
+      column_to_rownames(var = "sample") %>%
+      t()
+    
+    dds<-DESeqDataSetFromMatrix(countData = tData,
+                                colData = myTibble[[metaCol]][[i]],
+                                design = ~ dose)
+    dds <- estimateSizeFactors(dds)
+    dds <- counts(dds, normalized=TRUE) %>%
+      t()
+    
+    deSeqData[[i]] <- data.frame(nestData[[normCol]][[i]] %>% select(sample, dose), dds)
+      
+  }
+  
+  myTibble[[newCol]] <- deSeqData
+  
+  
+  
+  
+  
+  return(myTibble)
+}
+
+
+
+
+
+
+
+
+
+
 
 #### FUNCTION: logCount ####
 logCount <- function(x, zeroPad=1){
