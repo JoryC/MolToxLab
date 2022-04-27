38931e39d613fa5323f14b8d46f3b8f33f66d65c
tylernguyen1997 <tylernguyen@cmail.carleton.ca>
2022-04-27 15:04:02 -0400

    not sure why bmd script doesn't work...

---
+++ BPA and Alternatives Data/BMDExpressData_Archive/REACTOME/clean_files_GO_REACTOME.py
@@ -0,0 +1,32 @@
+import os
+
+
+def clean_file(filename):
+  with open(filename, 'r') as f:
+    s = f.read()
+    s = s[s.find('GO/Pathway/Gene Set/Gene ID'):]
+
+  with open(filename, 'w') as f:
+    f.write(s)
+ 
+
+def getfiles():
+  listing = os.listdir()
+  txts = []
+
+  for file in listing:
+    if file.endswith('.txt'):
+      txts.append(file)
+
+  return txts
+
+
+def main():
+  files = getfiles()
+
+  for file in files:
+    clean_file(file)
+
+
+if __name__ == '__main__':
+  main()
