6a01a849d15bba221a006dac77105bf9b483f61e
JoryC <jorycurry@cmail.carleton.ca>
2022-11-25 08:18:35 -0500

    Created a memory-efficient pipeline for testing and running 1000 bootstraps on behavioural data without any memory overflow with 16GB of DDR4 RAM

---
+++ Behaviour/Jory_Behaviour_R_Markdown/Compile_BMR_Results.R
@@ -0,0 +1,43 @@
+library(dplyr)
+
+# You must compile BMR results before you can compile SS resutlss
+#Please use runCompile_BMR_Results.sh to compile BMR results. It is a bash script that sources this R script in a loop
+
+args <- commandArgs() #list the arguments that were used on command line to open the script
+print(args)
+
+endpoint_R <- c("pearson", "spearman")
+
+endpoint <- endpoint_R[as.numeric(args[6])] #index which endpoint to use for this iteration of the loop after running the script with `runCompile_BMR_Results.sh`
+k=endpoint
+
+value <- c("totaldist") # Can add more endpoints to this like lardist, smldist, etc.
+sample_size <- 1000 #Change this to whatever sample size you want. Test with 10 or 100, get results with 1000 samples
+method <- c("rcurvep")
+
+rmarkdown::render(
+  input = paste0("estimate_bmr_", k, "_rcurvep.rmd"),
+  output_file = paste0("report_", "estimate_bmr_", k, "_rcurvep", "_", Sys.Date(), ".html"),
+  rmarkdown::html_document(
+    toc = TRUE,
+    toc_depth = 5,
+    toc_float = TRUE,
+    number_sections = TRUE,
+    code_folding = "show",
+    df_print = "paged",
+    code_download = TRUE,
+    theme = "readable"
+  ),
+  params = list(
+    value = value,
+    sample_size = sample_size,
+    endpoint = k,
+    method = method
+  )
+)
+print(paste0("Done", "report_", "estimate_bmr_", k, "_rcurvep", "_", Sys.Date(), ".html"))
+#Free up memory
+rm(list = ls())
+gc(reset = TRUE, full = TRUE)
+#rstudioapi::executeCommand("restartR")
+q(save = "no")
---
+++ Behaviour/Jory_Behaviour_R_Markdown/Compile_SS_Results.R
@@ -0,0 +1,45 @@
+library(dplyr)
+
+#Before Compiling_SS_Results, you first must compile BMR results using runCompile_BMR_results.sh
+#Please use runCompile_SS_Results.sh to compile all of the results. It is a bash script that sources this R script in a loop
+
+args <- commandArgs() #list the arguments that were used on command line to open the script
+print(args)
+
+endpoint_R <- c("pearson", "spearman")
+method_R <- c("rcurvep", "fitted")
+
+endpoint <- endpoint_R[as.numeric(args[6])] #index which endpoint to use for this iteration of the loop after running the script with `runCompile_SS_Results.sh`
+method <- method_R[as.numeric(args[7])] #index which method to use for this iteration of the loop after running the script with `runCompile_SS_Results.sh`
+i=endpoint
+j=method
+
+value <- c("totaldist") # Can add more endpoints to this like lardist, smldist, etc.
+sample_size <- 1000 #Change this to whatever sample size you want. Test with 10 or 100, get results with 1000 samples
+
+rmarkdown::render(
+  input = paste0(i, "_", j, ".rmd"),
+  output_file = paste0("report_", i, "_", j, "_", Sys.Date(), ".html"),
+  rmarkdown::html_document(
+    toc = TRUE,
+    toc_depth = 5,
+    toc_float = TRUE,
+    number_sections = TRUE,
+    code_folding = "show",
+    df_print = "paged",
+    code_download = TRUE,
+    theme = "readable"
+  ),
+  params = list(
+    value = value,
+    sample_size = sample_size,
+    endpoint = i,
+    method = j
+  )
+)
+print(paste0("Done", "report_", i, "_", j, "_", Sys.Date(), ".html"))
+#Free up memory
+rm(list = ls())
+gc(reset = TRUE, full = TRUE)
+#rstudioapi::executeCommand("restartR")
+q(save = "no")
